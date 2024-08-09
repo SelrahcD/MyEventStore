e9db617efd1780ba00c4aff3b447e290eac2d435
SelrahcD <c.desneuf@gmail.com>
2024-08-09 15:03:04 +0200

    Run test using a PostgreSQL container

--- MyDotNetEventStore.Tests/EventStoreTest.cs
+++ MyDotNetEventStore.Tests/EventStoreTest.cs
@@ -1,15 +1,45 @@
+using Npgsql;
+using Testcontainers.PostgreSql;
+
 namespace MyDotNetEventStore.Tests;
 
 public class EventStoreTests
 {
+    private readonly PostgreSqlContainer _postgresContainer = new PostgreSqlBuilder()
+        .Build();
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetup()
+    {
+        await _postgresContainer.StartAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        // Stop and dispose of the container
+        await _postgresContainer.DisposeAsync();
+    }
+
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void Test1()
+    public async Task TestDatabaseConnection()
     {
-        Assert.Pass();
+        var connectionString = _postgresContainer.GetConnectionString();
+
+        using (var connection = new NpgsqlConnection(connectionString))
+        {
+            await connection.OpenAsync();
+
+            using (var command = new NpgsqlCommand("SELECT 1", connection))
+            {
+                var result = await command.ExecuteScalarAsync();
+                Assert.AreEqual(1, result);
+            }
+        }
     }
 }
