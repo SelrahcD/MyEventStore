3448a14daba155a6fe131ee5c1e6929302a2cab4
SelrahcD <c.desneuf@gmail.com>
2024-08-09 14:54:47 +0200

    Setup project

---
+++ MyDotNetEventStore.Tests/EventStoreTest.cs
@@ -0,0 +1,15 @@
+namespace MyDotNetEventStore.Tests;
+
+public class EventStoreTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
