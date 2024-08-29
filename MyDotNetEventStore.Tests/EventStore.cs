aaeb2526c7d02f8c85c52b0825f7e562f72f5fa7
SelrahcD <c.desneuf@gmail.com>
2024-08-29 08:32:43 +0200

    Move to another file

--- MyDotNetEventStore.Tests/EventStore.cs
+++ MyDotNetEventStore.Tests/EventStore.cs
@@ -4,32 +4,6 @@ using NpgsqlTypes;
 
 namespace MyDotNetEventStore.Tests;
 
-public record StreamState
-{
-    public StreamStateType Type { get; }
-    public long ExpectedRevision { get; }
-
-    private StreamState(StreamStateType type, long expectedRevision)
-    {
-        Type = type;
-        ExpectedRevision = expectedRevision;
-    }
-
-    public static StreamState NoStream() => new(StreamStateType.NoStream, 0L);
-    public static StreamState Any() => new(StreamStateType.Any, 0L);
-    public static StreamState StreamExists() => new(StreamStateType.StreamExists, 0L);
-
-    public static StreamState AtRevision(long revision) => new StreamState(StreamStateType.AtRevision, revision);
-}
-
-public enum StreamStateType
-{
-    NoStream,
-    Any,
-    StreamExists,
-    AtRevision
-}
-
 public class ReadStreamResult : IEnumerable<ResolvedEvent>
 {
     private readonly ReadState _state;
---
+++ MyDotNetEventStore.Tests/StreamState.cs
@@ -0,0 +1,27 @@
+namespace MyDotNetEventStore.Tests;
+
+public enum StreamStateType
+{
+    NoStream,
+    Any,
+    StreamExists,
+    AtRevision
+}
+
+public record StreamState
+{
+    public StreamStateType Type { get; }
+    public long ExpectedRevision { get; }
+
+    private StreamState(StreamStateType type, long expectedRevision)
+    {
+        Type = type;
+        ExpectedRevision = expectedRevision;
+    }
+
+    public static StreamState NoStream() => new(StreamStateType.NoStream, 0L);
+    public static StreamState Any() => new(StreamStateType.Any, 0L);
+    public static StreamState StreamExists() => new(StreamStateType.StreamExists, 0L);
+
+    public static StreamState AtRevision(long revision) => new StreamState(StreamStateType.AtRevision, revision);
+}
