5bf08191006160275822bc819c5363368ba31193
SelrahcD <c.desneuf@gmail.com>
2024-09-10 09:08:25 +0200

    Make field public in AppendResult

--- MyDotNetEventStore.Tests/AppendResult.cs
+++ MyDotNetEventStore.Tests/AppendResult.cs
@@ -2,12 +2,12 @@ namespace MyDotNetEventStore.Tests;
 
 public record AppendResult
 {
-    private readonly long _position;
-    private readonly long _revision;
+    public readonly long Position;
+    public readonly long Revision;
 
     public AppendResult(long position, long revision)
     {
-        _position = position;
-        _revision = revision;
+        Position = position;
+        Revision = revision;
     }
 }
