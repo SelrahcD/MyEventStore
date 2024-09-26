a12c0ca97ac7f459671cdf772727b15f3bc33c63
SelrahcD <c.desneuf@gmail.com>
2024-09-26 15:29:29 +0200

    Extract event builder

--- MyDotNetEventStore.Tests/EventStoreTest.cs
+++ MyDotNetEventStore.Tests/EventStoreTest.cs
@@ -828,62 +828,11 @@ public class EventStoreTest
         Multiple
     }
 
-    private static T SelectRandom<T>(List<T> elements)
-    {
-        var random = new Random();
-        var randomIndex = random.Next(elements.Count);
-        return elements[randomIndex];
-    }
-
-
     private static EventBuilder AnEvent()
     {
         return new EventBuilder();
     }
 
-
-    public class EventBuilder
-    {
-        private readonly string _eventType;
-        private readonly string _data;
-        private readonly string _metadata;
-        private string _streamId;
-
-        public EventBuilder()
-        {
-            var fakeEventTypes = new List<string> { "event-type-1", "event-type-2", "event-type-3" };
-            var fakeEventData = new List<string> { "{}", "{\"id\": \"1234567\"}" };
-            var fakeEventMetaData = new List<string>
-                { "{}", "{\"userId\": \"u-345678\", \"causationId\": \"98697678\", \"correlationId\": \"12345\"}" };
-
-            _eventType = SelectRandom(fakeEventTypes);
-            _data = SelectRandom(fakeEventData);
-            _metadata = SelectRandom(fakeEventMetaData);
-            _streamId = "stream-&" + new Random().Next(1, 100);
-        }
-
-        public EventData ToEventData()
-        {
-            return new EventData(_eventType, _data, _metadata);
-        }
-
-        public ResolvedEvent ToResolvedEvent(int position, int revision)
-        {
-            return new ResolvedEvent(position, revision, _eventType, _data, _metadata, _streamId);
-        }
-
-        public string StreamId()
-        {
-            return _streamId;
-        }
-
-        public EventBuilder InStream(string streamId)
-        {
-            _streamId = streamId;
-
-            return this;
-        }
-    }
 
     private static async Task<List<T>> ToListAsync<T>(IAsyncEnumerable<T> asyncEnumerable)
     {
@@ -910,21 +859,72 @@ public class EventStoreTest
     }
 }
 
+public class EventBuilder
+{
+    private readonly string _eventType;
+    private readonly string _data;
+    private readonly string _metadata;
+    private string _streamId;
+
+    public EventBuilder()
+    {
+        var fakeEventTypes = new List<string> { "event-type-1", "event-type-2", "event-type-3" };
+        var fakeEventData = new List<string> { "{}", "{\"id\": \"1234567\"}" };
+        var fakeEventMetaData = new List<string>
+            { "{}", "{\"userId\": \"u-345678\", \"causationId\": \"98697678\", \"correlationId\": \"12345\"}" };
+
+        _eventType = SelectRandom(fakeEventTypes);
+        _data = SelectRandom(fakeEventData);
+        _metadata = SelectRandom(fakeEventMetaData);
+        _streamId = "stream-&" + new Random().Next(1, 100);
+    }
+
+    public EventData ToEventData()
+    {
+        return new EventData(_eventType, _data, _metadata);
+    }
+
+    public ResolvedEvent ToResolvedEvent(int position, int revision)
+    {
+        return new ResolvedEvent(position, revision, _eventType, _data, _metadata, _streamId);
+    }
+
+    public string StreamId()
+    {
+        return _streamId;
+    }
+
+    public EventBuilder InStream(string streamId)
+    {
+        _streamId = streamId;
+
+        return this;
+    }
+
+    private static T SelectRandom<T>(List<T> elements)
+    {
+        var random = new Random();
+        var randomIndex = random.Next(elements.Count);
+        return elements[randomIndex];
+    }
+
+}
+
 public static class EventBuilderExtensions
 {
-    public static object ToEventData(this OneOf<EventStoreTest.EventBuilder, List<EventStoreTest.EventBuilder>> oneOf)
+    public static object ToEventData(this OneOf<EventBuilder, List<EventBuilder>> oneOf)
     {
         return oneOf.Match<OneOf<EventData, List<EventData>>>(
             e => e.ToEventData(),
             e => e.ToEventData()).Value;
     }
 
-    public static List<EventData> ToEventData(this List<EventStoreTest.EventBuilder> eventBuilders)
+    public static List<EventData> ToEventData(this List<EventBuilder> eventBuilders)
     {
         return eventBuilders.Select(builder => builder.ToEventData()).ToList();
     }
 
-    public static List<ResolvedEvent> ToResolvedEvents(this List<EventStoreTest.EventBuilder> eventBuilders, int position = 1, int startRevision = 1)
+    public static List<ResolvedEvent> ToResolvedEvents(this List<EventBuilder> eventBuilders, int position = 1, int startRevision = 1)
     {
         var versions = new Dictionary<string, int>();
 
