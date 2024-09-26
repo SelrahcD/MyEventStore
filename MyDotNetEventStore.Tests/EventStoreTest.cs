33d24895bdfc456c178ca1c6d572cfe10ceb0fac
SelrahcD <c.desneuf@gmail.com>
2024-09-26 15:44:06 +0200

    Move to another file

--- MyDotNetEventStore.Tests/EventStoreTest.cs
+++ MyDotNetEventStore.Tests/EventStoreTest.cs
@@ -846,30 +846,3 @@ public class EventStoreTest
         return list;
     }
 }
-
-public static class AsyncEnumerableExtensions
-{
-    public static async Task<int> CountAsync<T>(this IAsyncEnumerable<T> asyncEnumerable)
-    {
-        var count = 0;
-
-        await foreach (var item in asyncEnumerable)
-        {
-            count++;
-        }
-
-        return count;
-    }
-
-    public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> asyncEnumerable)
-    {
-        var list = new List<T>();
-
-        await foreach (var item in asyncEnumerable)
-        {
-            list.Add(item);
-        }
-
-        return list;
-    }
-}
