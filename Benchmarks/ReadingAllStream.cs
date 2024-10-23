220234383563c47fd5a077292d6c3a53bb9d219f
SelrahcD <c.desneuf@gmail.com>
2024-10-23 15:49:12 +0200

    Add benchmark for reading all streams

---
+++ Benchmarks/ReadingAllStream.cs
@@ -0,0 +1,73 @@
+using BenchmarkDotNet.Attributes;
+using MyDotNetEventStore;
+using Npgsql;
+using Testcontainers.PostgreSql;
+
+namespace Benchmarks;
+
+public class ReadingAllStream
+{
+    private readonly PostgreSqlContainer _postgresContainer = new PostgreSqlBuilder()
+        .Build();
+
+    private EventStore _eventStore;
+    private List<EventData> _list;
+
+    private static NpgsqlConnection Connection;
+
+
+    [GlobalSetup]
+    public async Task GlobalSetup()
+    {
+        await _postgresContainer.StartAsync();
+
+        Connection = new NpgsqlConnection(_postgresContainer.GetConnectionString());
+
+        await Connection.OpenAsync();
+
+        var command = new NpgsqlCommand($"""
+                                         CREATE TABLE IF NOT EXISTS events (
+                                             position SERIAL PRIMARY KEY,
+                                             id UUID NOT NULL,
+                                             stream_id TEXT NOT NULL,
+                                             revision BIGINT NOT NULL,
+                                             event_type TEXT NOT NULL,
+                                             data JSONB,
+                                             metadata JSONB,
+                                             UNIQUE (stream_id, revision)
+                                         );
+                                         """, Connection);
+
+        await command.ExecuteNonQueryAsync();
+
+        _eventStore = new EventStore(Connection);
+
+        var random = new Random();
+
+        for (var i = 0; i < 10000; i++)
+        {
+            var streamId = random.NextInt64(1, 300);
+
+            await _eventStore.AppendAsync($"stream-{streamId}", new EventData("event-type", "{}", "{}"));
+        }
+    }
+
+    [GlobalCleanup]
+    public async Task GlobalCleanup()
+    {
+        await _postgresContainer.DisposeAsync();
+    }
+
+    [Params(Direction.Forward, Direction.Backward)]
+    public Direction Direction { get; set; }
+
+    [Benchmark]
+    public async Task ReadAllStream()
+    {
+        var readStreamResult = _eventStore.ReadAllAsync(Direction);
+
+        await foreach (var _ in readStreamResult)
+        {
+        }
+    }
+}
