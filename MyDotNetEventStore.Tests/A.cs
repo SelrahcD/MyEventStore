5d6eabab9cf3e3d5f124c511ed9f85d61175b2cf
SelrahcD <c.desneuf@gmail.com>
2024-11-04 11:27:53 +0100

    Extract clss

---
+++ MyDotNetEventStore.Tests/A.cs
@@ -0,0 +1,30 @@
+namespace MyDotNetEventStore.Tests;
+
+public static class A
+{
+    public static EventBuilders ListOfNEvents(int eventCount)
+    {
+        return ListOfNEvents(eventCount, (e) => e);
+    }
+
+    public static EventBuilders ListOfNEvents(int eventCount,
+        A.EventBuilderConfigurator eventBuilderConfiguratorConfigurator)
+    {
+        return ListOfNEvents(eventCount, eventBuilderConfiguratorConfigurator, EventBuilder.RevisionTracker());
+    }
+
+    public static EventBuilders ListOfNEvents(int eventCount,
+        A.EventBuilderConfigurator eventBuilderConfiguratorConfigurator, Dictionary<string, int> revisionTracker)
+    {
+        var eventBuilders = new List<EventBuilder>();
+        for (var i = 0; i < eventCount; i++)
+        {
+            eventBuilders.Add(eventBuilderConfiguratorConfigurator(new EventBuilder())
+                .WithCoherentRevisionsAndPositions(revisionTracker));
+        }
+
+        return new EventBuilders(eventBuilders);
+    }
+
+    public delegate EventBuilder EventBuilderConfigurator(EventBuilder eventBuilder);
+}
