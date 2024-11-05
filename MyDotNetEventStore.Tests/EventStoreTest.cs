a6fb37d8f79046d22bf8bd1ece8f9a1e2f93c5dc
SelrahcD <c.desneuf@gmail.com>
2024-11-05 11:41:56 +0100

    Make test smaller

--- MyDotNetEventStore.Tests/EventStoreTest.cs
+++ MyDotNetEventStore.Tests/EventStoreTest.cs
@@ -813,7 +813,7 @@ public class EventStoreTest
 
         [Test]
         public async Task returns_the_current_position_across_all_streams(
-            [Random(1, 1000, 1)] int eventCount)
+            [Random(1, 100, 1)] int eventCount)
         {
             var events = A.ListOfNEvents(eventCount);
 
